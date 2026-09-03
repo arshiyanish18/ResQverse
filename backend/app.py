c3d5c58c1f63b29bb0460d0fec66724b10ab8d98
arshiyanish18-boop <arshiyanish18@gmail.com>
2026-09-04 01:08:41 +0530

    Allow Vercel frontend in CORS

--- backend/app.py
+++ backend/app.py
@@ -9,10 +9,11 @@ app = FastAPI(title="RESQ Backend")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
+   allow_origins=[
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://res-qverse-nu.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
